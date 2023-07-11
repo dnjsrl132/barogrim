206911eb1651da2f7dac92b91c0ff7111fa2a8fe
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-07-11 14:41:03 +0900

    html include

--- django/ss/accountapp/views.py
+++ django/ss/accountapp/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello_world(request):
-    return HttpResponse('Hello word!')
+    return render(request,'ass.html')
+    #return HttpResponse('Hello word!')
