0146c3e532ab7f4e7551c14c6a59d1c5fced96ac
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-07-13 10:59:02 +0900

    20

--- django/pragmatic/accountapp/views.py
+++ django/pragmatic/accountapp/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from accountapp.models import HelloWorld
+from django.urls import reverse
 
-def hello_world(request):
-    return render(request,'accountapp/hello_world.html')
-    #return HttpResponse('hello world!')
 # Create your views here.
+def hello_world(request):
+    if request.method =="POST":
+        temp=request.POST.get('hello_world_input')
+        new_hello_world=HelloWorld()
+        new_hello_world.text=temp
+        new_hello_world.save()
+
+        hello_world_list=HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        #return render(request,'accountapp/hello_world.html',context={'hello_world_list':hello_world_list})
+    else:
+        hello_world_list=HelloWorld.objects.all()
+        return render(request,'accountapp/hello_world.html',context={'hello_world_list':hello_world_list})
+    #return HttpResponse('hello world!')
+
