1f9c5d536fa9d094e419b923d939790d84737cc8
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-07-19 14:18:30 +0900

    blog

--- django/pragmatic/blog/urls.py
+++ django/pragmatic/blog/urls.py
@@ -5,5 +5,7 @@ app_name ="blog"
 
 urlpatterns = [
     path('',blog,name='blog'),
+    path('newpost/',new_post,name='new_post'),
     path('<int:pk>/',posting,name='posting'),
+    path('<int:pk>/remove/',remove_post,name='remove'),
 ]
--- django/pragmatic/blog/views.py
+++ django/pragmatic/blog/views.py
@@ -13,5 +13,12 @@ def posting(request, pk):
     return render(request,'blog/posting.html',{'post':post})
 def new_post(request):
     if request.method =='POST':
+        Post.objects.create(postname=request.POST['postname'],contents=request.POST['contents'])
         return redirect('/blog/')
-    return render(request,'blog/new_post.html')
+    return render(request,'blog/newposting.html')
+def remove_post(request,pk):
+    post=Post.objects.get(pk=pk)
+    if request.method =='POST':
+        post.delete()
+        return redirect('/blog/')
+    return render(request,'blog/deleteposting.html',{'Post':post})
