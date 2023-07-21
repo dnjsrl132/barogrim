ca75f5c40432127f2dc6e2e642f94d226742475e
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-07-21 15:57:42 +0900

    delete, update

--- django/pragmatic/articleapp/models.py
+++ django/pragmatic/articleapp/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from projectapp.models import Project
 
+
+
 # Create your models here.
 class Article(models.Model):
     writer = models.ForeignKey(User,on_delete=models.SET_NULL, related_name='article',null=True)
@@ -11,3 +13,24 @@ class Article(models.Model):
     content = models.TextField(null=True)
 
     created_at = models.DateField(auto_now_add=True,null=True)
+
+    def delete(self, *args, **kwargs):
+        self.image.delete()
+        return super().delete(*args, **kwargs)
+    
+class Images(models.Model):
+    article_key = models.ForeignKey(Article,on_delete=models.SET_NULL, related_name='img',null=True)
+    user = models.CharField(primary_key=True, max_length=40)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    file_link = models.CharField(max_length=255, blank=True, null=True)
+    prompt = models.TextField(blank=True, null=True)
+    negative_prompt = models.TextField(blank=True, null=True)
+    timestamp = models.CharField(max_length=20, blank=True, null=True)
+    steps = models.IntegerField(blank=True, null=True)
+    sampler = models.CharField(max_length=50, blank=True, null=True)
+    cfg_scale = models.IntegerField(blank=True, null=True)
+    seed = models.CharField(max_length=10, blank=True, null=True)
+    model_hash = models.CharField(max_length=20, blank=True, null=True)
+    clip_skip = models.IntegerField(blank=True, null=True)
+    denoising_strength = models.CharField(max_length=10, blank=True, null=True)
+    img = models.TextField(blank=True, null=True)
