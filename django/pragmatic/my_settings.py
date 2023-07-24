87ec364565918c0a39c4452fb92fdef055f7fd99
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-07-24 16:18:18 +0900

    .

--- django/pragmatic/my_settings.py
+++ django/pragmatic/my_settings.py
@@ -4,8 +4,8 @@ DATABASES = {
         'NAME':'grim',
         'USER': 'grim',
         'PASSWORD': '1111',
-        'HOST': '3.39.195.100',
-        'PORT': '55447'
+        'HOST': '13.125.224.184',
+        'PORT': '57820'
     }
 }
 SECRET_KEY='django-insecure-#(z@*6yfj&lds*w6dgy4#v@$iq%*-b)jsw&un6lh01&)7lz3sh'
