4564b07028426744d10deae4fb8620fc1a0bcc8b
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-08-21 09:22:27 +0900

    bootstrap update

--- django/pragmatic/pragmatic/settings.py
+++ django/pragmatic/pragmatic/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accountapp',
     'view',
-    'bootstrap4',
+    'bootstrap5',
     'profileapp',
     'articleapp',
     'commentapp',
@@ -89,15 +89,15 @@ WSGI_APPLICATION = 'pragmatic.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
-DATABASES=my_settings.DATABASES
+
+#DATABASES=my_settings.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
